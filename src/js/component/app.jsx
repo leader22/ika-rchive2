0936e242cd5a13e7fed2ff2a25ba5a4e035eb85b
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-06-25 11:40:51 +0900

    header as fixed

--- src/js/component/app.jsx
+++ src/js/component/app.jsx
@@ -14,71 +14,70 @@ import type UserStore from '../store/user';
 import type Event from '../event';
 
 
-class App extends React.Component {
-  props: {
-    user: UserStore,
-    event: Event,
-  };
+const App = ({
+  user,
+  event,
+}: {
+  user: UserStore,
+  event: Event,
+}) => {
+  const { visibleTab } = user;
+  const {
+    onClickOpenAddLogModal,
+    onChangeTab,
+  } = event;
 
-  render() {
-    const { visibleTab } = this.props.user;
-    const {
-      onClickOpenAddLogModal,
-      onChangeTab,
-    } = this.props.event;
-
-    return (
-      <div className="app">
-        <header className="app-header">
-          <div className="app-header__title">
-            <img className="app-header__title__icon" src="./static/img/icon.png" alt="ウデマエアーカイブ2" />
-          </div>
-          <button
-            type="button"
-            className="app-header__right"
-            onClick={onClickOpenAddLogModal}
-          >
-            <span className="ft-ika">キロクする</span>
-          </button>
-        </header>
-        <Tabs
-          className="app-tabs"
-          selectedIndex={visibleTab}
-          onSelect={onChangeTab}
+  return (
+    <div>
+      <header className="app-header">
+        <div className="app-header__title">
+          <img className="app-header__title__icon" src="./static/img/icon.png" alt="ウデマエアーカイブ2" />
+        </div>
+        <button
+          type="button"
+          className="app-header__right"
+          onClick={onClickOpenAddLogModal}
         >
-          <TabPanel className="app-tabpanel">
-            <GraphPage />
-          </TabPanel>
-          <TabPanel className="app-tabpanel">
-            <StatPage />
-          </TabPanel>
-          <TabPanel className="app-tabpanel">
-            <LogPage />
-          </TabPanel>
-          <TabPanel className="app-tabpanel">
-            <OthersPage />
-          </TabPanel>
-          <TabList className="app-tablist">
-            <Tab className="app-tab" selectedClassName="app-tab--selected">
-              <span className="ft-ika">グラフ</span>
-            </Tab>
-            <Tab className="app-tab" selectedClassName="app-tab--selected">
-              <span className="ft-ika">トウケイ</span>
-            </Tab>
-            <Tab className="app-tab" selectedClassName="app-tab--selected">
-              <span className="ft-ika">ログ</span>
-            </Tab>
-            <Tab className="app-tab" selectedClassName="app-tab--selected">
-              <span className="ft-ika">ソノタ</span>
-            </Tab>
-          </TabList>
-        </Tabs>
-        <AddLogModal />
-        <ModLogModal />
-      </div>
-    );
-  }
-}
+          <span className="ft-ika">キロクする</span>
+        </button>
+      </header>
+      <Tabs
+        className="app-tabs"
+        selectedIndex={visibleTab}
+        onSelect={onChangeTab}
+      >
+        <TabPanel className="app-tabpanel">
+          <GraphPage />
+        </TabPanel>
+        <TabPanel className="app-tabpanel">
+          <StatPage />
+        </TabPanel>
+        <TabPanel className="app-tabpanel">
+          <LogPage />
+        </TabPanel>
+        <TabPanel className="app-tabpanel">
+          <OthersPage />
+        </TabPanel>
+        <TabList className="app-tablist">
+          <Tab className="app-tab" selectedClassName="app-tab--selected">
+            <span className="ft-ika">グラフ</span>
+          </Tab>
+          <Tab className="app-tab" selectedClassName="app-tab--selected">
+            <span className="ft-ika">トウケイ</span>
+          </Tab>
+          <Tab className="app-tab" selectedClassName="app-tab--selected">
+            <span className="ft-ika">ログ</span>
+          </Tab>
+          <Tab className="app-tab" selectedClassName="app-tab--selected">
+            <span className="ft-ika">ソノタ</span>
+          </Tab>
+        </TabList>
+      </Tabs>
+      <AddLogModal />
+      <ModLogModal />
+    </div>
+  );
+};
 
 export default inject(
   'event',
